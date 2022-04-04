67e920e7e446808b2a52702a51201bbd0ae8fd56
Ben Marden <benvmarden@gmail.com>
2022-04-03 21:52:38 -0700

    Add auth0 sdk and setup provider

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import React from 'react';
 
 const NavBar = () => {
@@ -6,11 +7,13 @@ const NavBar = () => {
       <p className="text-2xl font-bold text-gray-800">My Todos</p>
       <div className="flex">
         <a
-          href="/api/logout"
+          href="/api/auth/logout"
           className="mr-2 rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600">
           Logout
         </a>
-        <a href="/api/login" className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600">
+        <a
+          href="/api/auth/login"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600">
           Login
         </a>
       </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import { TodosProvider } from '@/contexts/TodosContext';
 import type { AppProps } from 'next/app';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <TodosProvider>
-      <div className="container mx-auto my-10 max-w-xl">
-        <Component {...pageProps} />
-      </div>
-    </TodosProvider>
+    <UserProvider>
+      <TodosProvider>
+        <div className="container mx-auto my-10 max-w-xl">
+          <Component {...pageProps} />
+        </div>
+      </TodosProvider>
+    </UserProvider>
   );
 }
 
