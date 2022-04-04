29ae21f5e0b6e073971392548784399f2fbc094d
Ben Marden <benvmarden@gmail.com>
2022-04-03 20:49:04 -0700

    Add change and click handlers for todos

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,24 @@
 import { TodosRecord } from '@/types/airtable';
 import React from 'react';
 import cn from 'classnames';
+import { useTodosContext } from '@/contexts/TodosContext';
 
 interface TodoProps {
   todo: TodosRecord;
 }
 
 const Todo = ({ todo }: TodoProps) => {
+  const { updateTodo, deleteTodo } = useTodosContext();
+
+  const handleToggleCompleted = async () => {
+    const updatedFields = {
+      ...todo.fields,
+      completed: !todo.fields.completed,
+    };
+    const updatedTodo = { id: todo.id, fields: updatedFields };
+    updateTodo(updatedTodo);
+  };
+
   return (
     <li className="my-2 flex items-center rounded-lg bg-white py-2 px-2 shadow-lg">
       <input
@@ -14,14 +26,18 @@ const Todo = ({ todo }: TodoProps) => {
         name="completed"
         id={`${todo.id}-completed`}
         checked={todo.fields.completed}
-        className="form-checkbox mr-2"
+        onChange={handleToggleCompleted}
+        className="form-checkbox mr-2 rounded-sm"
       />
       <p className={cn({ 'line-through': todo.fields.completed }, 'flex-1 text-gray-800')}>
         {todo.fields.description}
       </p>
       <button
         type="button"
-        className="bg-red-500 py-1 text-sm text-white hover:bg-red-600 px-2 rounded">Delete</button>
+        className="rounded bg-red-500 py-1 px-2 text-sm text-white hover:bg-red-600"
+        onClick={() => deleteTodo(todo.id)}>
+        Delete
+      </button>
     </li>
   );
 };
